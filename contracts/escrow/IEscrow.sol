088a16c1baa6ea4cf3ebb6bb6e038f9c23685f3e
Frankc <63744530+francescocirulli@users.noreply.github.com>
2023-01-25 15:44:14 +0100

    Create IEscrow.sol

---
+++ contracts/escrow/IEscrow.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+contract IEscrow {
+    event PaidRate(uint256 time, uint256 amount);
+    event FailedPaidRate(uint256 time);
+    event CollateralWithdrawn(uint256 amount);
+    event CollateralAdded(uint256 amount);
+    event ClosedLoan(uint256 time, uint256 amount);
+    event Received(address, uint256);
+}
