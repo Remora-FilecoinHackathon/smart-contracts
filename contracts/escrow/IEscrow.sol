15c7d87b34e76bedbc2f15f645117105e6e05e04
Frankc <63744530+francescocirulli@users.noreply.github.com>
2023-01-26 14:44:53 +0100

    Update IEscrow.sol

--- contracts/escrow/IEscrow.sol
+++ contracts/escrow/IEscrow.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
-
 contract IEscrow {
     event PaidRate(uint256 time, uint256 amount);
     event FailedPaidRate(uint256 time);
@@ -8,4 +7,9 @@ contract IEscrow {
     event CollateralAdded(uint256 amount);
     event ClosedLoan(uint256 time, uint256 amount);
     event Received(address, uint256);
+
+    struct BigInt {
+    bytes val;
+    bool neg;
+    } 
 }
