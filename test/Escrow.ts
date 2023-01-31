43a6d594e03a0a7e6017a7437e32c4f944456967
Kalpita Mandal <kalpitamandal@Kalpitas-MacBook-Air.local>
2023-01-31 23:00:30 +0530

    Adding rest of the tests

--- test/Escrow.ts
+++ test/Escrow.ts
@@ -46,30 +46,139 @@ describe("Escrow Contract", function () {
 
         var loanKey = await lenderManager.loanKeys(0);
 
-        var escrow = "";
-
-        lenderManager.on(
-            "BorrowOrder",
-            async (
-            escrowAddress: string,
-            loanAmount: number,
-            lenderAmountAvailable: number,
-            startBlock: number,
-            amountToPay: number,
-            key: number,
-            minerActor: any
-            ) => {
-            escrow = escrowAddress;
-            console.log("Escrow Address deployed at ", escrow);
-            }
+        priorityFee = await callRpc("eth_maxPriorityFeePerGas");
+        await lenderManager
+        .connect(otherAccount)
+        .createBorrow(
+          loanKey,
+          ethers.utils.parseEther("0.001"),
+          addressAsBytes(MINER_ADDRESS),
+          {
+            maxPriorityFeePerGas: priorityFee.result,
+          }
         );
 
-        return { lenderManager, escrow, ORACLE_ADDRESS, MINER_ADDRESS, owner, otherAccount };
+        const escrowAddress = await lenderManager.escrowContracts(loanKey,0);
+        console.log("Escrow Address",escrowAddress);
+
+        const escrowContract = await ethers.getContractAt("Escrow", escrowAddress);
+
+        return { lenderManager, escrowContract, loanKey, ORACLE_ADDRESS, MINER_ADDRESS, owner, otherAccount };
     }
 
     describe("Deployments", function () {
-        it("Should deploy to a valid address", async function () {
-            const { escrow } = await loadFixture(deployEscrowFixture);
+        it("Should deploy to a valid (non-address) address", async function () {
+            const { escrowContract } = await loadFixture(deployEscrowFixture);
+
+            expect(escrowContract.address).to.not.equal(0);
+        })
+
+        it("All params should be updated correctly", async function () {
+            const { lenderManager, MINER_ADDRESS, escrowContract, otherAccount, owner, loanKey } = await loadFixture(deployEscrowFixture);
+            
+            // Param update checks
+            const lender = (await escrowContract.lender()).toString();
+            const borrower = (await escrowContract.borrower()).toString();
+            const minerActor = (await escrowContract.minerActor()).toString();
+            const loanAmount = await escrowContract.loanAmount();
+            const rateAmount = await escrowContract.rateAmount();
+            const withdrawInterval = await escrowContract.withdrawInterval();
+            const end = await escrowContract.end();
+            
+            const endTime = (await lenderManager.positions(loanKey)).endTimestamp;
+            const interestRate = (await lenderManager.positions(loanKey)).interestRate;
+            const interest = await lenderManager.calculateInterest(ethers.utils.parseEther("0.001"), interestRate);
+
+            expect(lender).to.equal(owner.address);
+            expect(borrower).to.equal(otherAccount.address);
+            // expect(minerActor).to.equal(addressAsBytes(MINER_ADDRESS).toString());
+            expect(loanAmount).to.equal(interest[1]);
+            expect(rateAmount).to.equal(interest[0]);
+            expect(withdrawInterval).to.equal(30 * 86400);
+            expect(end).to.equal(endTime);
+        })
+    })
+
+    describe("Start Loan", function () {
+        it("Should fail if the loan is already started", async function () {
+            const { lenderManager, escrowContract } = await loadFixture(deployEscrowFixture);
+        })
+
+        it("Test start loan", async function () {
+            const { lenderManager, escrowContract } = await loadFixture(deployEscrowFixture);
+            await escrowContract.startLoan();
+        })
+    })
+
+    describe("Transfer to miner", function () {
+        it("Borrower should not be able to call the function", async function () {
+            const { escrowContract } = await loadFixture(deployEscrowFixture);
+
+            await expect(escrowContract.transferToMinerActor(10)).to.be.revertedWithCustomError(escrowContract,"Not_The_Borrower");
+        })
+
+        it("Shaould fail for invalid amount", async function () {
+            const { escrowContract, otherAccount } = await loadFixture(deployEscrowFixture);
+
+            await expect(escrowContract.connect(otherAccount).transferToMinerActor(ethers.utils.parseEther("0.003"))).to.be.revertedWithCustomError(escrowContract,"Not_Enough_Balance");
+        })
+
+        it("Should pass for correct params sent to the function", async function () {
+            const { escrowContract, otherAccount } = await loadFixture(deployEscrowFixture);
+
+            await escrowContract.connect(otherAccount).transferToMinerActor(ethers.utils.parseEther("0.0001"));
+        })
+
+    })
+
+    describe("Transfer from miner", function () {
+
+    })
+
+    describe("Repay", function () {
+        it("Should fail if repaying non-existant loan", async function () {
+            const { escrowContract } = await loadFixture(deployEscrowFixture);
+
+            var nextWithdraw = await escrowContract.nextWithdraw();
+            console.log(nextWithdraw);
+
+            await escrowContract.repay();
+
+            nextWithdraw = await escrowContract.nextWithdraw();
+            console.log(nextWithdraw);
+        })
+    })
+
+    describe("Withdraw before Loan Start", function () {
+        it("Should fail if the lender is not calling the function", async function () {
+            const { escrowContract, otherAccount } = await loadFixture(deployEscrowFixture);
+
+            // Should fail when borrower calls
+            await expect(escrowContract.connect(otherAccount).withdrawBeforLoanStarts()).to.be.revertedWithCustomError(escrowContract, "Not_The_Lender");
+        })
+
+        it("Should fail if loan is already started", async function () {
+            const { escrowContract } = await loadFixture(deployEscrowFixture);
+
+            await escrowContract.startLoan();
+
+            // Should fail when borrower calls
+            await expect(escrowContract.withdrawBeforLoanStarts()).to.be.revertedWithCustomError(escrowContract, "Already_Started");
+        })
+
+        it("Should pass if conditions are correct", async function () {
+            const { escrowContract } = await loadFixture(deployEscrowFixture);
+
+            await expect(escrowContract.withdrawBeforLoanStarts()).to.emit(escrowContract, "ClosedLoan");
+        })
+    })
+
+    describe("Close Loan", function () {
+        it("Should fail if loan is not started", async function () {
+            const { escrowContract } = await loadFixture(deployEscrowFixture);
+
+            // deadline not reached yet
+            await expect(escrowContract.closeLoan()).to.be.revertedWithCustomError(escrowContract, "Loan_Not_Expired");
         })
     })
 })
