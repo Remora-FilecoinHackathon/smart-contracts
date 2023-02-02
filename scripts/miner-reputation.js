0bbc875165637be95aaddcf1d6c5bdf0153d7085
0xThresh.eth <0xthresh@protonmail.com>
2023-02-01 19:35:13 -0800

    Failing transaction on miner-reputation.js

--- scripts/miner-reputation.js
+++ scripts/miner-reputation.js
@@ -1,39 +1,19 @@
+const { boolean } = require("hardhat/internal/core/params/argumentTypes");
+const axios = require("axios");
 const ethers = require("hardhat").ethers;
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+const ENDPOINT_ADDRESS = "https://api.hyperspace.node.glif.io/rpc/v1";
 
 async function callRpc(method, params) {
     const res = await axios.post(ENDPOINT_ADDRESS, {
-      jsonrpc: "2.0",
-      method: method,
-      params: params,
-      id: 1,
-    });
+        jsonrpc: "2.0",
+        method: method,
+        params: params,
+        id: 1,
+      });
     return res.data;
   }
-
-
-async function writeReputation() {
-    const MINER_ADDRESS =
-      "t3wj7cikpzptshfuwqleehoytar2wcvom42q6io7lopbl2yp2kb2yh3ymxovsd5ccrgm36ckeibzjl3s27pzuq";
-    
-    // set to my own wallet so I can write reputation 
-    const ORACLE_ADDRESS = "0xc2b60CfFe4f20b2046C951CDEB459aF897cff571";
-    
-    // NEW ADDRESS - uses mock contracts 
-    const LENDER_MANAGER_ADDRESS = "0x1a75831c8646fC5c4a6Bf6337c696dE14F0c85b6";
-  
-    var priorityFee = await callRpc("eth_maxPriorityFeePerGas");
-    const LenderManager = await ethers.getContractFactory("LenderManager");
-    const lenderManager = LenderManager.attach(LENDER_MANAGER_ADDRESS);
-
-    // receiveReputation score takes 1 for bad, 2 for good
-    await lenderManager.receiveReputationScore(1, 2, {
-        maxPriorityFeePerGas: priorityFee.result,
-      });
-  
-    priorityFee = await callRpc("eth_maxPriorityFeePerGas");
-}
 
 
 function getData(apiUrl, params) {
@@ -52,14 +32,56 @@ function getData(apiUrl, params) {
     });
 }
 
+function determineIfMinerIsReputable(jsonData) {
+    var jsonData = jsonData['miners'];
+    var minerIsReputable = false;
+    var minerReputation = jsonData[0].score;
+    var minerReachable = jsonData[0].reachability;
+    console.log('REPUTATION SCORE');
+    console.log(minerReputation);
+    console.log('REACHABLE');
+    console.log(minerReachable);
 
-function getMinerAddress(jsonData) {
-    var jsonData = jsonData['miners']
-    var minerAddresses = [];
-    for (var i = 0; i < jsonData.length; i++) {
-        minerAddresses.push(jsonData[i].address);
+    if (minerReputation > 95 && minerReachable === 'reachable') {
+        minerIsReputable = true;
+        return minerIsReputable;
     }
-    return minerAddresses;
+    else {
+        return minerIsReputable;
+    }
+}
+
+
+async function writeReputation(isMinerReputable) {
+    const MINER_ADDRESS =
+      "t3wj7cikpzptshfuwqleehoytar2wcvom42q6io7lopbl2yp2kb2yh3ymxovsd5ccrgm36ckeibzjl3s27pzuq";
+    
+    // set to my own wallet so I can write reputation 
+    const ORACLE_ADDRESS = "0xc2b60CfFe4f20b2046C951CDEB459aF897cff571";
+    
+    // NEW ADDRESS - uses mock contracts 
+    const LENDER_MANAGER_ADDRESS = "0x1a75831c8646fC5c4a6Bf6337c696dE14F0c85b6";
+  
+    var priorityFee = await callRpc("eth_maxPriorityFeePerGas");
+    const LenderManager = await ethers.getContractFactory("LenderManager");
+    const lenderManager = LenderManager.attach(LENDER_MANAGER_ADDRESS);
+    const id = await lenderManager.currentId()
+    const oracle = await lenderManager.oracle()
+    console.log("CURRENT ID");
+    console.log(id);
+    console.log(oracle);
+    // receiveReputation score takes 1 for bad, 2 for good
+    if (isMinerReputable === false) {
+        await lenderManager.receiveReputationScore(id, 1, {
+            gasLimit: 1000000000,
+            maxPriorityFeePerGas: priorityFee.result,
+          });
+    } else if (isMinerReputable === true) {
+        await lenderManager.receiveReputationScore(id, 2, {
+            gasLimit: 1000000000,
+            maxPriorityFeePerGas: priorityFee.result,
+          });
+    }
 }
 
 
@@ -68,11 +90,12 @@ async function main(address) {
         var minerData = await getData('https://api.filrep.io/api/v1/miners', `?search=${address}`);
         // var minerData = await getData('https://api.filrep.io/api/v1/miners', '?limit=10&sortBy=noPenalties');
         minerData = JSON.parse(minerData)   
-        var reputableMiners = getMinerAddress(minerData)
-        console.log("ALL DATA");
-        console.log(minerData);
-        console.log("MINER ADDRESS");
-        console.log(reputableMiners);
+        var minerReputable = determineIfMinerIsReputable(minerData);
+        console.log("MINER REPUTABLE?");
+        console.log(minerReputable);
+
+        var txn = await writeReputation(minerReputable);
+        console.log(txn);
         
     } catch (error) {
         console.log(error);
@@ -80,4 +103,4 @@ async function main(address) {
 }
 
 // In Lambda, this address will be passed via the event listener 
-main('f066104');
+main('f01662887');
